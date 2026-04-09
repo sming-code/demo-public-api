8131b87092f027a36c4ec277ac1ff2a80e795539
Matt Astill <matt.astill@tag-group.com>
2026-04-09 15:44:48 +0100

    Testing again

--- SmingCode.Utilities.ProcessTracking.WebApi/ProcessTrackingHeadersHelper.cs
+++ SmingCode.Utilities.ProcessTracking.WebApi/ProcessTrackingHeadersHelper.cs
@@ -1,11 +1,25 @@
+using Microsoft.Extensions.Logging;
+
 namespace SmingCode.Utilities.ProcessTracking.WebApi;
 
 internal static class ProcessTrackingHeadersHelper
 {
     internal static ProcessTrackingDetail GetProcessTrackingDetailFromHeader(
-        this HttpRequest httpRequest
+        this HttpRequest httpRequest,
+        ILogger<ProcessTrackingHeaderMiddleware> logger
     )
     {
+        logger.LogInformation("Got in here.");
+        logger.LogInformation(
+            "Headers are: {HeaderInfo}",
+            string.Join(
+                ",",
+                httpRequest.Headers.Select(header =>
+                    $"{header.Key}:{header.Value}"
+                )
+            )
+        );
+
         if (!(
             httpRequest.Headers.TryGetValue(Configuration.PROCESS_ID_HEADER_NAME, out var processIdHeaderVal)
             && httpRequest.Headers.TryGetValue(Configuration.CORRELATION_ID_HEADER_NAME, out var correlationIdHeaderVal)
@@ -13,6 +27,12 @@ internal static class ProcessTrackingHeadersHelper
         {
             throw new Exception();
         }
+
+        logger.LogInformation(
+            "Parsed headers are {header1} and {header2}",
+            processIdHeaderVal,
+            correlationIdHeaderVal
+        );
 
         return new(
             processIdHeaderVal!,
