9901f7e13343887e11a3173b35156facad30ccb4
Matt Astill <matt.astill@tag-group.com>
2026-05-01 16:39:55 +0100

    Updating middleware

--- DemoApp.PublicApi.Dependencies/Apis/Customer/CustomerApi.cs
+++ DemoApp.PublicApi.Dependencies/Apis/Customer/CustomerApi.cs
@@ -1,6 +1,7 @@
 using SmingCode.Utilities.ServiceApiClient;
 
 namespace DemoApp.PublicApi.Dependencies.Apis.Customer;
+using Models;
 
 internal class CustomerApi(
     IServiceApiClient<CustomerApi> _serviceApiClient
@@ -9,9 +10,9 @@ internal class CustomerApi(
     public async Task<Guid> CreateCustomer(
         string firstName,
         string surname
-    ) => await _serviceApiClient.Post<(string FirstName, string Surname), Guid>(
+    ) => await _serviceApiClient.Post<CreateCustomerModel, Guid>(
             string.Empty,
-            ( firstName, surname )
+            new (firstName, surname)
         );
 
     public async Task<CustomerDto[]> GetAllCustomers()
---
+++ DemoApp.PublicApi.Dependencies/Apis/Customer/Models/CreateCustomerModel.cs
@@ -0,0 +1,6 @@
+namespace DemoApp.PublicApi.Dependencies.Apis.Customer.Models;
+
+public record CreateCustomerModel(
+    string FirstName,
+    string Surname
+);
